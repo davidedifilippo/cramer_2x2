2bc9009fedeb3fe0ce1da1477bea687c18c02067
Ing. Di Filippo <davidedifilippo@gmail.com>
2021-12-01 19:55:06 +0100

    Create cramer_lib.h
    libreria per calcolo dei determinanti di Cramer 2x2

---
+++ cramer_lib.h
@@ -0,0 +1,11 @@
+int Det(int a1,int b1,int a2,int b2){
+return a1*b2-a2*b1;
+}
+
+int DX(int c1, int c2, int b1, int b2){
+return (c1*b2-c2*b1);
+}
+
+int DY(int a1, int a2, int c1, int c2){
+return a1*c2-a2*c1;
+}
